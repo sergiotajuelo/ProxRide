bd1f3e72e323f634961c72bb93baa8d8ab1bde66
Sergio <sergiotajuelo1@gmail.com>
2025-09-14 16:40:28 +0200

    Basic project structure

---
+++ proxride-backend/src/test/java/es/sergiotajuelo/proxride/backend/ProxrideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sergiotajuelo.proxride.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxrideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
